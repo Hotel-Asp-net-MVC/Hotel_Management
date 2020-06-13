84eabf39afb9028dc3e9533b6c38a2bd46e185b0
phamduycuong <17520309@gm.uit.edu.vn>
2020-06-13 10:27:43 +0700

    báo cáo

---
+++ HotelManagement/Models/BaoCao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelManagement.Models
+{
+    public class BaoCao
+    {
+        public string MaDP { get; set; }
+        public string TenKH { get; set; }
+        public string TenNV { get; set; }
+        public int SoPhong { get; set; }
+        public string NgayDen { get; set; }
+        public string NgayDi { get; set; }
+        public double Gia { get; set; }
+
+        public BaoCao() { }
+        public BaoCao(string madp, string tenkh, string tennv, int sophong, string ngayden, string ngaydi, double gia)
+        {
+            this.MaDP = madp;
+            this.TenKH = tenkh;
+            this.TenNV = tennv;
+            this.SoPhong = sophong;
+            this.NgayDen = ngayden;
+            this.NgayDi = ngaydi;
+            this.Gia = gia;
+        }
+    }
+}
