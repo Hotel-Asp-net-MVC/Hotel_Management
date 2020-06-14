4f103b9fde2305eee764eb231b1113ead6056c67
NgocHien <ngochienpr2014@gmail.com>
2020-06-14 08:46:25 +0700

    update ui

---
+++ HotelManagement/Controllers/BaoCaoController.cs
@@ -0,0 +1,48 @@
+﻿using HotelManagement.DaTa_Access_Object;
+using HotelManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelManagement.Controllers
+{
+    public class BaoCaoController : Controller
+    {
+        // GET: BaoCao
+        public ActionResult BaoCao()
+        {
+            string ngayden = Request.Form["ngayden"];
+            string ngaydi = Request.Form["ngaydi"];
+            if(ngayden!=null&& ngaydi!=null)
+            {
+                string[] arrngayden = ngayden.Split('-');
+                string[] arrngaydi = ngaydi.Split('-');
+                BaoCaoDAO baocao_dao = new BaoCaoDAO();
+                List<BaoCao> bc = new List<BaoCao>();
+                bc = baocao_dao.GetAllBaoCao(arrngayden[0], arrngayden[1], arrngaydi[1], arrngayden[2], arrngaydi[2]);
+                ViewBag.baocao = bc;
+            }
+            
+
+            
+            return View();
+        }
+        public ActionResult CT_BaoCao()
+        {
+            string ngayden = Request.Form["ngayden"];
+            string ngaydi = Request.Form["ngaydi"];
+            string[] arrngayden = ngayden.Split('-');
+            string[] arrngaydi = ngaydi.Split('-');
+
+            BaoCaoDAO baocao_dao = new BaoCaoDAO();
+            List<BaoCao> bc = new List<BaoCao>();
+            bc = baocao_dao.GetAllBaoCao(arrngayden[0], arrngayden[1], arrngaydi[1], arrngayden[2], arrngaydi[2]);
+            ViewBag.baocao = bc;
+            ViewBag.ngayden = ngayden;
+            ViewBag.ngaydi = ngaydi;
+            return View("BaoCao");
+        }
+    }
+}
---
+++ HotelManagement/DaTa_Access_Object/BaoCaoDAO.cs
@@ -0,0 +1,41 @@
+﻿
+using HotelManagement.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelManagement.DaTa_Access_Object
+{
+    public class BaoCaoDAO
+    {
+        //lấy tất cả thông tin để xuất báo cáo
+        public List<BaoCao> GetAllBaoCao(string nam, string thangden, string thangdi,string ngayden, string ngaydi)
+        {
+            List<BaoCao> ListBaoCao = new List<BaoCao>();
+
+            Connect_Database connect = new Connect_Database();
+            MySqlConnection mySql = connect.Connection();
+            //string sql = "SELECT vdatphong.MaDP, vkhachhang.TenKH, vnhanvien.TenNV, vphong.SoPhong, vct_datphong.NgayDen, vct_datphong.NgayDi,vhoadon.Gia" +
+            //    " from vkhachhang, vdatphong, vct_datphong , vhoadon, vnhanvien, vphong WHERE vkhachhang.MaKH=vdatphong.MaKH AND vdatphong.MaDP=vct_datphong.MaDP AND vkhachhang.MaKH=vhoadon.MaKHThanhToan AND vnhanvien.MaNV=vhoadon.MaNV AND vphong.MaPhong=vdatphong.MaPhong and year(vhoadon.NgayThanhToan)='"+nam+"' AND month(vhoadon.NgayThanhToan)>='"+thangden+"' and month(vhoadon.NgayThanhToan)<='"+thangdi+"' AND day(vhoadon.NgayThanhToan)>='"+ngayden+"' and day(vhoadon.NgayThanhToan)<='"+ngaydi+"'";
+
+            string sql = "SELECT * " +
+                " from vkhachhang, vdatphong, vct_datphong , vhoadon, vnhanvien, vphong WHERE vkhachhang.MaKH=vdatphong.MaKH AND vdatphong.MaDP=vct_datphong.MaDP AND vkhachhang.MaKH=vhoadon.MaKHThanhToan AND vnhanvien.MaNV=vhoadon.MaNV AND vphong.MaPhong=vdatphong.MaPhong and year(vhoadon.NgayThanhToan)='" + nam + "' AND month(vhoadon.NgayThanhToan)>='" + thangden + "' and month(vhoadon.NgayThanhToan)<='" + thangdi + "' AND day(vhoadon.NgayThanhToan)>='" + ngayden + "' and day(vhoadon.NgayThanhToan)<='" + ngaydi + "'";
+
+            MySqlCommand command = new MySqlCommand(sql, mySql);
+            
+            using (var reader = command.ExecuteReader())
+            {
+                
+                while (reader.Read())
+                {
+                   
+                    BaoCao BC = new BaoCao((string)reader["MaDP"], (string)reader["TenKH"], (string)reader["TenNV"], (int)reader["SoPhong"], ((DateTime)reader["NgayDen"]).ToString(), ((DateTime)reader["NgayDi"]).ToString(), (double)reader["Gia"]);
+                    ListBaoCao.Add(BC);
+                }
+            }
+            return ListBaoCao;
+        }
+    }
+}
